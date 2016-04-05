ccc3b3f090754560309d78f5ced184d10cb2e3e8
mkt <markus.kottlaender@crea.de>
2016-04-05 15:41:37 +0200

    added tooltips

--- templates/template.php
+++ templates/template.php
@@ -8,7 +8,10 @@ include 'header.php';
         $path = '';
         foreach (array_filter(explode('/', $vars['dir'])) as $subdir) {
             $path .= '/' . $subdir;
-            ?><a class="uk-button" href="<?php echo $this->getUrl('template', ['dir' => trim($path, '/')]); ?>"><?php echo $subdir; ?></a> <?php
+            ?><a class="uk-button"href="<?php echo $this->getUrl(
+                'template',
+                ['dir' => trim($path, '/')]
+            ); ?>"><?php echo $subdir; ?></a> <?php
         }
         ?>
     </h2>
@@ -25,23 +28,37 @@ include 'header.php';
         <?php
         /** @var Symfony\Component\Finder\SplFileInfo $file */
         foreach ($vars['finder'] as $file) {
-            ?><tr>
-                <td>
-                    <?php
-                    if ($file->isDir()) {
-                        ?><a href="<?php echo $this->getUrl('template', ['dir' => trim($vars['dir'] . '/' . $file->getRelativePathname(), '/')]) ?>" class="uk-button uk-width-1-1 uk-text-left"><i class="uk-icon-folder-o"></i> <?php echo $file->getRelativePathname(); ?></a> <?php
-                    } else {
-                        ?><span style="padding: 0 12px; line-height: 30px;"><i class="uk-icon-file-code-o"></i> <?php echo $file->getRelativePathname(); ?></span> <?php
-                    }
-                    ?>
-                </td>
-                <td style="width: 50px; line-height: 30px;">
-                    <?php echo $file->getExtension(); ?>
-                </td>
-                <td class="uk-text-right" style="width: 80px;">
-                    <a href="<?php echo $this->getUrl('template_edit', ['file' => trim($vars['dir'] . '/' . $file->getRelativePathname(), '/')]) ?>" class="uk-button uk-button-success"><i class="uk-icon-edit"></i></a>
-                    <a href="#" class="uk-button uk-button-danger"><i class="uk-icon-trash"></i></a>
-                </td>
+            ?>
+            <tr>
+            <td>
+                <?php
+                if ($file->isDir()) {
+                    ?><ahref="<?php echo $this->getUrl(
+                        'template',
+                        ['dir' => trim($vars['dir'] . '/' . $file->getRelativePathname(), '/')]
+                    ) ?>" class="uk-button uk-width-1-1 uk-text-left"><i
+                        class="uk-icon-folder-o"></i> <?php echo $file->getRelativePathname(); ?></a> <?php
+                } else {
+                    ?><span style="padding: 0 12px; line-height: 30px;"><i
+                        class="uk-icon-file-code-o"></i> <?php echo $file->getRelativePathname(); ?></span> <?php
+                }
+                ?>
+            </td>
+            <td style="width: 50px; line-height: 30px;">
+                <?php echo $file->getExtension(); ?>
+            </td>
+            <td class="uk-text-right" style="width: 80px;">
+                <a href="<?php echo $this->getUrl(
+                    'template_edit',
+                    ['file' => trim($vars['dir'] . '/' . $file->getRelativePathname(), '/')]
+                ) ?>"
+                   class="uk-button uk-button-success" data-uk-tooltip title="Template bearbeiten">
+                    <i class="uk-icon-edit"></i>
+                </a>
+                <a href="#" class="uk-button uk-button-danger"  data-uk-tooltip title="Template löschen">
+                    <i class="uk-icon-trash"></i>
+                </a>
+            </td>
             </tr><?php
         }
         ?>
