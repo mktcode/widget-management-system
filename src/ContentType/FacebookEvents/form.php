edf58972d929400996135753757d5176a3f4c66a
mkt <markus.kottlaender@crea.de>
2016-04-18 13:45:45 +0200

    made fields required

--- src/ContentType/FacebookEvents/form.php
+++ src/ContentType/FacebookEvents/form.php
@@ -1,17 +1,17 @@
 <label>
     Fanpage-URL<br>
-    <input type="url" class="uk-width-1-1" value="<?php echo $content ? $this->getData('url', $content->getId()) : ''; ?>" name="url" placeholder="http://..."/>
+    <input type="url" class="uk-width-1-1" value="<?php echo $content ? $this->getData('url', $content->getId()) : ''; ?>" name="url" placeholder="http://..." required/>
 </label>
 <label>
     Anzahl Events<br>
-    <input type="number" class="uk-width-1-1" value="<?php echo $content ? $this->getData('events', $content->getId()) : ''; ?>" name="events" placeholder=""/>
+    <input type="number" class="uk-width-1-1" value="<?php echo $content ? $this->getData('events', $content->getId()) : ''; ?>" name="events" placeholder="" required/>
 </label>
 <label>
     FB-App ID<br>
-    <input type="text" class="uk-width-1-1" value="<?php echo $content ? $this->getData('appID', $content->getId()) : ''; ?>" name="appID" placeholder=""/>
+    <input type="text" class="uk-width-1-1" value="<?php echo $content ? $this->getData('appID', $content->getId()) : ''; ?>" name="appID" placeholder="" required/>
 </label>
 
 <label>
     FB-App Secret<br>
-    <input type="text" class="uk-width-1-1" value="<?php echo $content ? $this->getData('appSecret', $content->getId()) : ''; ?>" name="appSecret" placeholder=""/>
+    <input type="text" class="uk-width-1-1" value="<?php echo $content ? $this->getData('appSecret', $content->getId()) : ''; ?>" name="appSecret" placeholder="" required/>
 </label>
